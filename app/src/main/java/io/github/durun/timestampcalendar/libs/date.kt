f79432116ef1dbbaf3c669deb8af91db8be8c6ef
Durun <negiigainuki@gmail.com>
2022-05-08 20:54:41 +0900

    rfc3339DateFormatter

--- app/src/main/java/io/github/durun/timestampcalendar/libs/date.kt
+++ app/src/main/java/io/github/durun/timestampcalendar/libs/date.kt
@@ -1,6 +1,10 @@
 package io.github.durun.timestampcalendar.libs
 
+
 import java.time.format.DateTimeFormatter
 import java.util.*
 
-val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy' at 'hh:mma", Locale("en"))
+val dateFormatter: DateTimeFormatter =
+    DateTimeFormatter.ofPattern("MMMM dd, yyyy' at 'hh:mma", Locale("en"))
+val rfc3339DateFormatter: DateTimeFormatter =
+    DateTimeFormatter.ofPattern("yyyy-MM-dd'T'h:m:ss.SZ", Locale("en"))
